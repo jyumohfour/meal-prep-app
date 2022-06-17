a846dc8a52710b0ad3dd9520aae7fb6fffb4a9dc
Ananay Joshi <ananayjo22@gmail.com>
2022-06-16 22:42:03 -0700

    works mf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// importing components from react-router-dom package
+import {
+BrowserRouter as Router,
+Routes,
+Route,
+Navigate,
+} from "react-router-dom";
+
+// import Home component
+import Home from "./elements/Home";
+// import About component
+import About from "./elements/About";
+// import ContactUs component
+import ContactUs from "./elements/ContactUs";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+	<>
+	{/* This is the alias of BrowserRouter i.e. Router */}
+	<Router>
+		<Routes>
+      {/* This route is for home component
+      with exact path "/", in component props
+      we passes the imported component*/}
+      <Route path="/" element={<Home />} />
+        
+      {/* This route is for about component
+      with exact path "/about", in component
+      props we passes the imported component*/}
+      <Route path="/about" element={<About />} />
+        
+      {/* This route is for contactus component
+      with exact path "/contactus", in
+      component props we passes the imported component*/}
+      <Route path="/contactus" element={<ContactUs />} />
+        
+      {/* If any route mismatches the upper
+      route endpoints then, redirect triggers
+      and redirects app to home component with to="/" */}
+
+      <Route path="*" element={<Navigate to ="/" />} />
+		</Routes>
+	</Router>
+	</>
+);
 }
 
 export default App;
