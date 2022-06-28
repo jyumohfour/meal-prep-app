80b870d819eadde4bae6f904effa510dd967e82d
Ananay Joshi <ananayjo22@gmail.com>
2022-06-27 20:54:36 -0700

    test-push

--- src/elements/Home.jsx
+++ src/elements/Home.jsx
@@ -1,32 +1,43 @@
 import React from "react";
 import "./styles.css";
+import "./navbar.css";
 // importing Link from react-router-dom to navigate to
 // different end points.
 import { Link } from "react-router-dom";
 import salad from "./images/salad.jpeg";
-//import "./styles.css";
+
 
 
 const Home = () => {
 return (
 	
 		<div img alt = "salad" className = "homeImg" src = {salad} >
-		<h1>Home Page</h1>
-		{/* <img alt="salad" className="homeImg" src={salad} /> */}
-		<br />
-		<ul>
+			<div className="home">
 			<li>
 			{/* Endpoint to route to Home element */}
 			<Link to="/">Home</Link>
 			</li>
+			</div>
+			<div className="about">
 			<li>
 			{/* Endpoint to route to About element */}
 			<Link to="/about">About</Link>
 			</li>
+			</div>
+			<div className="contactUs">
 			<li>
 			{/* Endpoint to route to Contact Us element */}
 			<Link to="/contactus">Contact Us</Link>
 			</li>
+			</div>
+			
+		{/* <img alt="salad" className="homeImg" src={salad} /> */}
+		<br />
+		<ul>
+			
+			
+			
+			
 			<li>
 			{/*Endpoint to route to List element */}
 			<Link to="/list">List</Link>
