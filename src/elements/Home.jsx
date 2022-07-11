46748fb698d398d41c42e0e37f55694bf16f8ffe
jyumohfour <jsya2020@mymail.pomona.edu>
2022-07-11 17:56:38 -0500

    created nav bar + temp logo

--- src/elements/Home.jsx
+++ src/elements/Home.jsx
@@ -4,44 +4,46 @@ import "./styles.css";
 // different end points.
 import { Link } from "react-router-dom";
 import salad from "./images/salad.jpeg";
+import penguin from "./images/penguin.jpeg";
 //import "./styles.css";
 
 
 const Home = () => {
 return (
-	
 		<div img alt = "salad" className = "homeImg" src = {salad} >
-		<h1>Meal Prep</h1>
-		{/* <img alt="salad" className="homeImg" src={salad} /> */}
-		<br />
-		<ul>
-			<li>
-			{/* Endpoint to route to Home element */}
-			<Link to="/">Home</Link>
-			</li>
-			<li>
-			{/* Endpoint to route to About element */}
-			<Link to="/about">About</Link>
-			</li>
-			<li>
-			{/* Endpoint to route to Contact Us element */}
-			<Link to="/contactus">Contact Us</Link>
-			</li>
-			<li>
-			{/*Endpoint to route to List element */}
-			<Link to="/list">List</Link>
-			</li>
-			<li>
-			<Link to="/about">
-     			<button type="button">
-          			Click Me!
-     			</button>
- 			</Link>
-			</li>
-			<li>
-				<Link to="/checkbox">checkbox</Link>
-			</li>
-		</ul>
+			<ul class="navBar">
+				<div className="logo">
+					<img alt="penguin" className="logoImg" src={penguin}/>
+					<text>Meal Prep</text>
+				</div>
+				<li>
+					{/* Endpoint to route to Home element */}
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					{/* Endpoint to route to About element */}
+					<Link to="/about">About</Link>
+				</li>
+				<li>
+					{/* Endpoint to route to Contact Us element */}
+					<Link to="/contactus">Contact Us</Link>
+				</li>
+				<li>
+					{/*Endpoint to route to List element */}
+					<Link to="/list">List</Link>
+				</li>
+				<li>
+					<Link to="/about">
+						<button type="button">
+							Click Me!
+						</button>
+					</Link>
+				</li>
+				<li>
+					<Link to="/checkbox">checkbox</Link>
+				</li>
+			</ul>
+			
 		<div className="home-title">
 			Meal prep made easy
 		</div>
