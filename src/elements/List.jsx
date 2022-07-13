f7185fb9ec759dc3a16f13d0db31770f07db04ec
jyumohfour <jsya2020@mymail.pomona.edu>
2022-07-12 19:36:10 -0500

    generated custom endpoint link on "list" page

--- src/elements/List.jsx
+++ src/elements/List.jsx
@@ -14,12 +14,18 @@ const List = () => {
   //     }
   //   )
   // },[])
+  const origLink = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=23139cc3494244e986af3e4ec60c0d9b'
   const queryOne= JSON.parse(sessionStorage.getItem('usedIngredients'));
+  console.log(queryOne);
   var stringOne = "usedIngredients=";
   const queryTwo = JSON.parse(sessionStorage.getItem('diets'));
+  console.log(queryTwo);
   var stringTwo = "diet=";
   const queryThree = JSON.parse(sessionStorage.getItem('intolerances'));
+  console.log(queryThree);
   var stringThree = "intolerances=";
+
+  var finalLink = "";
 
   for(let i = 0; i < queryOne.length; i++) {
     stringOne+= i < queryOne.length-1 ? queryOne[i] + "," : queryOne[i]
@@ -32,12 +38,40 @@ const List = () => {
   for (let i = 0; i < queryThree.length; i++) {
     stringThree += i < queryThree.length - 1 ? queryThree[i] + "," : queryThree[i]
   }
+
+  if(queryOne.length == 0) {
+    if(queryTwo.length == 0 && queryThree.length == 0) {
+      finalLink = origLink;
+    }
+    else if(queryTwo.length == 0) {
+      finalLink = origLink + "&" + stringThree;
+    }
+    else if(queryThree.length == 0) {
+      finalLink = origLink + "&" + stringTwo;
+    }
+    else {
+      finalLink = origLink + "&" + stringTwo + "&" + stringThree;
+    }
+  }
+  else if(queryTwo.length == 0) {
+    finalLink = origLink + "&" + stringOne;
+    if(queryThree.length != 0 ) {
+      finalLink += "&" + stringThree;
+    }
+  }
+  else if(queryThree.length == 0) {
+    finalLink = origLink + "&" + stringOne + "&" + stringTwo;
+  }
+  else {
+    finalLink = origLink + "&" + stringOne + "&" + stringTwo + "&" + stringThree;
+  }
+
+  sessionStorage.setItem('bigLink', finalLink);
+
   return (
 
     <div>
-      {stringOne}
-      {stringTwo}
-      {stringThree}
+      {finalLink}
       {/* <ul>{list.map(item =>
         <li> {item}</li>
       )}
--- src/elements/List2/SecondList.jsx
+++ src/elements/List2/SecondList.jsx
@@ -4,26 +4,7 @@ import "./second.css";
 
 function SecondList() {
   const [mealData, setMealData] = useState(null);
-
-  const usedIngredients = JSON.parse(sessionStorage.getItem('usedIngredients'));
-  var ingred = "usedIngredients=";
-  const diets = JSON.parse(sessionStorage.getItem('diets'));
-  var diet = "diet=";
-  const intolerances = JSON.parse(sessionStorage.getItem('intolerances'));
-  var intol = "intolerances=";
-
-  for (let i = 0; i < usedIngredients.length; i++) {
-    ingred += i < usedIngredients.length - 1 ? usedIngredients[i] + "," : usedIngredients[i]
-  }
-
-  for (let i = 0; i < diets.length; i++) {
-    diet += i < diets.length - 1 ? diets[i] + "," : diets[i]
-  }
-
-  for (let i = 0; i < intolerances.length; i++) {
-    intol += i < intolerances.length - 1 ? intolerances[i] + "," : intolerances[i]
-  }
-
+  
   function getMealData() {
     // api key options:
     // josh's main one: 23139cc3494244e986af3e4ec60c0d9b
@@ -32,13 +13,13 @@ function SecondList() {
     // random dude's: 9b6b788926b9411fa6efee53289091c0
     // remember to update api key on "SecondList" and "Meal"
     fetch(
-      `https://api.spoonacular.com/recipes/complexSearch?apiKey=23139cc3494244e986af3e4ec60c0d9b&${ingred}&${diet}&${intol}`
+      sessionStorage.getItem('bigLink')
+      // `https://api.spoonacular.com/recipes/complexSearch?apiKey=23139cc3494244e986af3e4ec60c0d9b&${ingred}&${diet}&${intol}`
       // `https://api.spoonacular.com/recipes/complexSearch?apiKey=cb1c464d94f142c08b156c5beddade8b&number=3&includeIngredients=${ingredients}`
     )
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        console.log(`https://api.spoonacular.com/recipes/complexSearch?apiKey=23139cc3494244e986af3e4ec60c0d9b&${ingred}&${diet}&${intol}`);
         setMealData(data);
       })
       .catch(() => {
