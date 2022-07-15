3fcbd7fdb403635189ab2a85dca5364748da05c7
jyumohfour <jsya2020@mymail.pomona.edu>
2022-07-15 16:42:00 -0500

    collapsible window includes image + text

--- src/elements/List.jsx
+++ src/elements/List.jsx
@@ -6,17 +6,18 @@ import penguin from "./images/penguin.jpeg";
 const List = () => {
   var cake = "https://sugarspunrun.com/wp-content/uploads/2019/01/Best-Cheesecake-Recipe-2-1-of-1-4.jpg";
 
+  // collapsible function
   function modifyImageSize(props) {
     var img = document.getElementById(props);
     if(img !== null) {
       // initial click: makes image smaller
-      if(img.style.height === "400px") {
+      if(img.style.height === "600px") {
         img.style.height = "100px";
         img.style.transition = "height 0.5s ease";
       }
       // next click: makes image larger
       else{
-        img.style.height = "400px";
+        img.style.height = "600px";
         img.style.transition = "height 0.5s ease";
       }
     }
@@ -32,6 +33,7 @@ const List = () => {
       <li className="imageList">
         <ul className="cropped" id="balls" >
           <img alt="penguin" onClick={() => modifyImageSize("balls")} src={penguin}/>
+          <text>WE GOT DAWGS</text>
         </ul>
         <ul className="cropped" id="mf" >
           <img alt="new" onClick={() => modifyImageSize("mf")} src={cake} />
